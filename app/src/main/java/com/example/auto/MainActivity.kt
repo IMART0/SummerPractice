08b604c4cb31a7b24c2522587c8b396f4beeac51
IMART0 <imartirosyan@vk.com>
2024-07-01 22:00:02 +0300

    empty project

--- app/src/main/java/com/example/auto/MainActivity.kt
+++ app/src/main/java/com/example/auto/MainActivity.kt
@@ -10,9 +10,6 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private var startButton: Button? = null
-    private var sendNumber: EditText? = null
-
     private var viewBinding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,45 +17,6 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding?.root)
-        initViews()
+
     }
-
-    private fun initViews() {
-        viewBinding?.let { binding ->
-            binding.startButton.setOnClickListener {
-                start(binding.numberEditText.text.toString().toInt())
-            }
-        }
-    }
-
-    private fun start(count: Int) {
-        val lap: ArrayList<Transport> = ArrayList()
-        for (i in 1..count)
-            lap.add(TranportCreator.createRandomTranport())
-        race(lap)
-    }
-
-    private fun race(lap: ArrayList<Transport>) {
-        if (lap.size == 2) {
-            println("Гонка между ${lap[0].modelName} и ${lap[1].modelName}, " +
-                    "Победитель ${faster(lap[0], lap[1]).modelName}")
-        } else {
-            val newLap = ArrayList<Transport>()
-            while (lap.size > 1) {
-                val t1 = lap.removeAt(Random.nextInt(0, lap.size))
-                val t2 = lap.removeAt(Random.nextInt(0, lap.size))
-                newLap.add(faster(t1, t2))
-                println("Гонка между ${t1.modelName} и ${t2.modelName}, " +
-                        "Победитель ${faster(t1, t2).modelName}")
-                if (lap.size == 1) {
-                    println("${lap[0].modelName} - Нет пары, следующий круг")
-                    newLap.add(lap.removeAt(0))
-                }
-            }
-            return race(newLap)
-        }
-    }
-
-    private fun faster(t1: Transport, t2: Transport): Transport = if (t1.maxSpeed >= t2.maxSpeed) t1 else t2
-
 }
